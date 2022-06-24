4f883e578a6db91d63a1c5163ad657e932748250
PetrChatrny <xchatrny@mendelu.cz>
2022-06-24 11:23:26 +0200

    initialized backend

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// connect db
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("INFO - MongoDB connected successfully."))
+    .catch((err) => console.log(`ERROR - MongoDB not connected : ${err} `));
+
+// run server
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+});
