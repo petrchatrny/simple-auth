95712e1b2d76e27128d4ecd217239d28e92dec63
PetrChatrny <xchatrny@mendelu.cz>
2022-06-25 15:46:41 +0200

    initialized frontend

--- app.js
+++ app.js
@@ -19,9 +19,16 @@ mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology:
     .then(() => console.log("INFO - MongoDB connected successfully."))
     .catch((err) => console.log(`ERROR - MongoDB not connected : ${err} `));
 
-// register routes
+// register api routes
 const apiRoutes = require("./routes");
+const path = require("path");
 app.use("/api", apiRoutes)
+
+// register SPA routes
+app.use(express.static(path.join(__dirname + "/public")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 // run server
 const port = process.env.PORT || 3000
