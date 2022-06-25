d2b83303d4545abc08166e31ee246c6b2f34f6e7
PetrChatrny <xchatrny@mendelu.cz>
2022-06-25 17:59:34 +0200

    Updated user model

--- model/user.js
+++ model/user.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const user = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
