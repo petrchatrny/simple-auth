71134f5ed3ea0df7a47688246b6b099624822eaf
PetrChatrny <xchatrny@mendelu.cz>
2022-06-26 02:15:25 +0200

    implemented protecting of routes in frontend

--- frontend/src/services/router.js
+++ frontend/src/services/router.js
@@ -3,6 +3,8 @@ import HomeView from "@/views/HomeView";
 import DragonsView from "@/views/DragonsView";
 import LoginView from "@/views/LoginView";
 import RegisterView from "@/views/RegisterView";
+import store from "@/services/store";
+import UserView from "@/views/UserView";
 
 const routes = [
     {
@@ -10,7 +12,9 @@ const routes = [
         name: "home",
         component: HomeView,
         meta: {
-            title: "Epic dragons"
+            title: "Epic dragons",
+            authRequired: false,
+            authForbidden: false
         }
     },
     {
@@ -18,23 +22,39 @@ const routes = [
         name: "dragons",
         component: DragonsView,
         meta: {
-            title: "Dragons"
-        }
+            title: "Dragons",
+            authRequired: true,
+            authForbidden: false
+        },
     },
     {
         path: "/login",
         name: "login",
         component: LoginView,
         meta: {
-            title: "Login"
-        }
+            title: "Login",
+            authRequired: false,
+            authForbidden: true
+        },
     },
     {
         path: "/register",
         name: "register",
         component: RegisterView,
         meta: {
-            title: "Register"
+            title: "Register",
+            authRequired: false,
+            authForbidden: true
+        },
+    },
+    {
+        path: "/user",
+        name: "user",
+        component: UserView,
+        meta: {
+            title: "User profile",
+            authRequired: true,
+            authForbidden: false
         }
     }
 ]
@@ -42,6 +62,24 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
+});
+
+// routes protection
+router.beforeEach(async (to, from, next) => {
+    // update store if needed
+    if (!store.state.isStoreUpdated) {
+        await store.dispatch("updateStore")
+    }
+
+    // control routes
+    const isAuthenticated = store.state.userLoggedIn;
+    if (!isAuthenticated && to.meta.authRequired) {
+        next({name: "login"})
+    } else if (isAuthenticated && to.meta.authForbidden) {
+        next({name: "user"})
+    } else {
+        next()
+    }
 });
 
 router.afterEach((to) => {
---
+++ frontend/src/views/UserView.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>User</h1>
+</template>
+
+<script>
+export default {
+  name: "UserView",
+  data() {
+    return {}
+  },
+  computed: {
+    user() {
+      return this.$store.state.user
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
